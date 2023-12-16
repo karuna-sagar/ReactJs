3e985d0f77db0eb1043f69230cf9d7287bbe3bdc
Karuna sagar <karunasagar129130@gmail.com>
2023-12-16 23:25:01 +0530

    Add header, menu, and footer components to the pizza app.

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -48,18 +48,38 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React !</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Header />
+      <Menu />
+      <Footer />
     </div>)
 }
+function Header() {
+  return (
+    <div>
+      <h1>Fast React Pizza Co.</h1>
+    </div>
+  )
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+    </div>
+  )
+}
+function Footer() {
+  return <footer> {new Date().toLocaleTimeString()} We're currently open</footer>
+}
+
 function Pizza() {
   return (
     <div>
+      <img src="pizzas/spinaci.jpg" alt="pizza Spinaci" />
       <h2>Pizza Spinaci</h2>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-      <img src="pizzas/spinaci.jpg" alt="pizza Spinaci" />
     </div>
   )
 }

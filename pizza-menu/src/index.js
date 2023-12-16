02fdea0d4d69fa50dd400de007fbb91ffc32de1d
Karuna sagar <karunasagar129130@gmail.com>
2023-12-16 23:33:32 +0530

    Add logic to display the current time and whether the restaurant is open or not.

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -71,6 +71,12 @@ function Menu() {
   )
 }
 function Footer() {
+  const hour = new Date().getHours();
+  console.log(hour)
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
   return <footer> {new Date().toLocaleTimeString()} We're currently open</footer>
 }
 

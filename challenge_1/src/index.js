7e8c0291dffe40b6d2311a5cdd5c8a7db23df0c6
Karuna sagar <karunasagar129130@gmail.com>
2023-12-17 19:42:20 +0530

    Add styling and components for the challenge.

--- challenge_1/src/index.js
+++ challenge_1/src/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+function Image() {
+  return (
+    <img src="images/focaccia.jpg" alt="focaccia" />
+  )
+}
+function Intro() {
+  return (
+    <div>
+      <h1> Sagar Singh </h1>
+      <p>
+        I'm a straightforward, kind, and self-assured guy. My father is a
+        well-known physician who understands how to embrace his work with ardor
+        and commitment.
+      </p>
+    </div>
+  )
+}
+
+function SkillList() {
+
+}
+function App() {
+  return (
+    <div className="card">
+      <Image />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +41,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+

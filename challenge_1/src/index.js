f638ebfa7d4ed250dfefd7bcfa0b0ef8784cc1ce
Karuna sagar <karunasagar129130@gmail.com>
2023-12-17 19:44:06 +0530

    Add skills data for the user's profile.

--- challenge_1/src/index.js
+++ challenge_1/src/index.js
@@ -2,6 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA"
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D"
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF"
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33"
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB"
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00"
+  }
+];
 function Image() {
   return (
     <img src="images/focaccia.jpg" alt="focaccia" />

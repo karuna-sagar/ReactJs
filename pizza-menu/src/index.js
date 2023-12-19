1c872dd077605c8e0b24d8d66921ec6c5daa284c
Karuna sagar <karunasagar129130@gmail.com>
2023-12-19 20:03:15 +0530

    Changed the pizza component to add a sold-out class when the pizza is sold out, and to display "SOLD OUT" instead of the price when the pizza is sold out.

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -126,12 +126,12 @@ function Footer() {
 
 function Pizza({ pizza }) {
   return (
-    <div className='pizza'>
+    <div className={`pizza ${pizza.soldOut ? 'sold-out' : ''}`}>
       <img src={pizza.photoName} alt={pizza.name} />
       <div>
         <h3>{pizza.name}</h3>
         <p>{pizza.ingredients}</p>
-        <span> {pizza.price} </span>
+        <span> {pizza.soldOut ? 'SOLD OUT' : pizza.price} </span>
       </div>
     </div>
   )

f216ac2d20e6123e2f88d39d1d28b55e718f50cd
Karuna sagar <karunasagar129130@gmail.com>
2023-12-21 02:50:36 +0530

    Changed the buttons to increment/decrement a step value, and added buttons to increment/decrement the count by the step value.

--- challenge_3/src/App.js
+++ challenge_3/src/App.js
@@ -12,14 +12,20 @@ export default function App() {
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(0);
   const date = new Date("jan 22 2027")
   date.setDate(date.getDate() + count)
   return (
     <div>
       <div>
-        <button onClick={() => setCount((c) => c - 1)}>-</button>
-        <span>Count={count}</span>
-        <button onClick={() => setCount((c) => c + 1)}>+</button>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span>Step:{Math.abs(step)}</span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count:{Math.abs(count)}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <p>
         <span>{count === 0 ? "Today is " : count > 0 ? `${count} days from today ` : `${Math.abs(count)} days ago was `}</span>

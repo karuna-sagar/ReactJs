673641bd661e8bc99d3a21af125dcb9ee3058003
Karuna sagar <karunasagar129130@gmail.com>
2023-12-21 02:45:44 +0530

    Add date calculation and display to the counter component.

--- challenge_3/src/App.js
+++ challenge_3/src/App.js
@@ -1,8 +1,7 @@
-
-import { useState } from 'react';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Counter />
@@ -10,14 +9,25 @@ function App() {
   );
 }
 
+
 function Counter() {
   const [count, setCount] = useState(0);
-  <div>
+  const date = new Date("jan 22 2027")
+  date.setDate(date.getDate() + count)
+  return (
     <div>
-      <button onClick={() => setCount((c) => c - 1)}>-</button>
-      <div>Count={count}</div>
-      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <div>
+        <button onClick={() => setCount((c) => c - 1)}>-</button>
+        <span>Count={count}</span>
+        <button onClick={() => setCount((c) => c + 1)}>+</button>
+      </div>
+      <p>
+        <span>{count === 0 ? "Today is " : count > 0 ? `${count} days from today ` : `${Math.abs(count)} days ago was `}</span>
+        <span>{date.toDateString()}</span>
+      </p>
     </div>
-  </div>
+  )
 }
-export default App;
+
+
+

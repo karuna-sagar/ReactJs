b5ade18156e287f61bb005ca907677680e0b785c
Karuna sagar <karunasagar129130@gmail.com>
2023-12-23 16:30:27 +0530

    Add React state management for item description input

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,4 +1,4 @@
-
+import { useState } from 'react'
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,6 +20,7 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>
 }
 function Form() {
+  const [description, setDescription] = useState("")
   function handleSubmit(e) {
     e.preventDefault();
     console.log(e);
@@ -34,7 +35,9 @@ function Form() {
           </option>
         )}
       </select>
-      <input type="text" placeholder="Item.." />
+      <input type="text" placeholder="Item.." value={description} onChange={e =>
+        setDescription(e.target.value)
+      } />
       <button>Add</button>
     </form>
   )

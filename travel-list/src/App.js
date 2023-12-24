d46b1248057cc2774d922e75cf3e8c0edecdf5c5
Karuna sagar <karunasagar129130@gmail.com>
2023-12-24 17:37:08 +0530

    Add logging statements to the handleToggleItem function.

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -14,7 +14,9 @@ export default function App() {
     setItems(items => items.filter(item => item.id !== id))
   }
   function handleToggleItem(id) {
+
     setItems(items => items.map(item => item.id === id ? { ...item, packed: !item.packed } : item))
+
   }
   return (
     <div className='app'>

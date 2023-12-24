dad1e22048af6ced1cdef4f7120a8b8fe95daeae
Karuna sagar <karunasagar129130@gmail.com>
2023-12-25 01:26:41 +0530

    Changed the buttons to use a custom component called Button. Mainly focus on children

---
+++ steps/src/App-v1.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+];
+
+
+export default function App() {
+    // const step = 1;
+    const [step, setStep] = useState(1);
+    const [isOpen, setIsOpen] = useState(true);
+    // const [test, setTest] = useState({ name: "Sagar" });
+    function handlePrevious() {
+        // if (step > 1) setStep(step - 1);
+        if (step > 1) setStep((s) => s - 1);
+    }
+    function handleNext() {
+        if (step < 3) setStep((s) => s + 1);
+        // if (step < 3) setStep(step + 1);
+        // setTest({ name: "singh" })
+    }
+    // onClick={() => setIsOpen(!isOpen)}>
+    return (
+
+        <><button className="close" onClick={() => setIsOpen((is) => !is)}>&times;</button>{
+
+            isOpen && (
+
+                <div className="steps">
+                    <div className="numbers">
+                        <div className={step >= 1 ? 'active' : ""}>1</div>
+                        <div className={step >= 2 ? 'active' : ""}>2</div>
+                        <div className={step >= 3 ? 'active' : ""}>3</div>
+                    </div>
+                    <p className="message">
+                        Step {step}: {messages[step - 1]}
+                        {/* {test.name} */}
+                    </p>
+                    <div className="buttons">
+                        <button style={{ backgroundColor: "#7950f2", color: "#FFF" }} onClick={handlePrevious}>Previous</button>
+                        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handleNext}>Next</button>
+                    </div>
+                </div>
+            )
+        }
+        </>
+    )
+}

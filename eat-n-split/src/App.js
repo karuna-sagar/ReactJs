264ba43f39df9a1f0de917038bf5fcf26bdaa856
Karuna sagar <karunasagar129130@gmail.com>
2023-12-26 19:37:52 +0530

    Update FriendList and Friend components to display friend information and balances.

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -30,7 +30,30 @@ export default function App() {
 }
 
 function FriendList() {
+  const friends = initialFriends;
   return (
-    <ul>List</ul>
+    <ul>
+      {friends.map(friend => <Friend friend={friend} key={friend.key} />)}
+    </ul>
+  )
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.id} />
+      <h3>{friend.name} </h3>
+      {friend.balance < 0 &&
+        <p className="red">You owe {friend.name} ${Math.abs(friend.balance)} </p>
+      }
+      {friend.balance > 0 &&
+        <p className="green">{friend.name} owes you ${Math.abs(friend.balance)} </p>
+      }
+      {friend.balance === 0 &&
+        <p >You and  {friend.name} are even ${Math.abs(friend.balance)} </p>
+      }
+      <button className="button">Select</button>
+
+    </li>
   )
 }

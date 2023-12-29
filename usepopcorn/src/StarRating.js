412422e6a3d4a63fcd660bf208142d101aa3e1d1
Karuna sagar <karunasagar129130@gmail.com>
2023-12-30 02:24:14 +0530

    Add messages prop to StarRating component and display the corresponding message based on the rating.

--- usepopcorn/src/StarRating.js
+++ usepopcorn/src/StarRating.js
@@ -12,7 +12,7 @@ const starContainerStyle = {
 }
 
 
-export default function StarRating({ maxRating = 5, color = "#fcc419", size = 48 }) {
+export default function StarRating({ maxRating = 5, color = "#fcc419", size = 48, messages = [] }) {
   const [rating, setRating] = useState(0);
   const [tempRating, setTempRating] = useState(0);
   function handleRating(rating) {
@@ -32,7 +32,7 @@ export default function StarRating({ maxRating = 5, color = "#fcc419", size = 48
           <Star key={i} onRate={() => handleRating(i + 1)} full={tempRating ? tempRating >= i + 1 : rating >= i + 1} onHoverIn={() => setTempRating(i + 1)} onHoverOut={() => setTempRating(0)} color={color} size={size} />
         )}
       </div>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      <p style={textStyle}>{messages.length === maxRating ? messages[tempRating ? tempRating - 1 : rating - 1] : tempRating || rating || ""}</p>
     </div>
   )
 }
--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -6,7 +6,7 @@ import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StarRating />
+    <StarRating message={['Terrible', "Bad", "Okay", "Good", "Amazing"]} />
     {/* <App /> */}
   </React.StrictMode>
 );

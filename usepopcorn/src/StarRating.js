008bcd0e964a2b611446364b58556b5724bcf746
Karuna sagar <karunasagar129130@gmail.com>
2023-12-30 01:06:49 +0530

    Update StarRating component to use tempRating state for hover effect

--- usepopcorn/src/StarRating.js
+++ usepopcorn/src/StarRating.js
@@ -16,7 +16,8 @@ const textStyle = {
   margin: "0"
 }
 export default function StarRating() {
-  const [rating, setRating] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
   function handleRating(rating) {
     setRating(rating)
   }
@@ -25,10 +26,10 @@ export default function StarRating() {
       <div style={starContainerStyle} >
 
         {Array.from({ length: 5 }, (_, i) =>
-          <Star key={i} onRate={() => handleRating(i + 1)} full={rating >= i + 1} />
+          <Star key={i} onRate={() => handleRating(i + 1)} full={tempRating ? tempRating >= i + 1 : rating >= i + 1} onHoverIn={() => setTempRating(i + 1)} onHoverOut={() => setTempRating(0)} />
         )}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   )
 }
@@ -40,10 +41,10 @@ const starStyle = {
   display: 'block',
   cursor: 'pointer'
 }
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
 
   return (
-    <span role="button" style={starStyle} onClick={onRate}>
+    <span role="button" style={starStyle} onClick={onRate} onMouseEnter={onHoverIn} onMouseLeave={onHoverOut}>
       {full ?
 
         <svg

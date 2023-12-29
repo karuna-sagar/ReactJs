0c0bf5cb31d9b414b7ab58e5787691d20a19afcf
Karuna sagar <karunasagar129130@gmail.com>
2023-12-30 02:30:04 +0530

    Changed the prop name from `message` to `messages` in the `StarRating` component.

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -6,7 +6,7 @@ import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StarRating message={['Terrible', "Bad", "Okay", "Good", "Amazing"]} />
+    <StarRating messages={['Terrible', "Bad", "Okay", "Good", "Amazing"]} />
     {/* <App /> */}
   </React.StrictMode>
 );

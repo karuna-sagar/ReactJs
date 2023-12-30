1d9f39131ae8e09b09a1948d40e7cc8992ab054d
Karuna sagar <karunasagar129130@gmail.com>
2023-12-30 23:05:28 +0530

    Fetch movie data from OMDB API and display in the app

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,10 +50,17 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
 
+
+
+const KEY = '43035ac2'
+export default function App() {
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  useEffect(function () {
+
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=43035ac2&s=interstellar`).then(res => res.json()).then(data => setMovies(data.Search))
+  }, [])
   return (
     <>
       <NavBar>

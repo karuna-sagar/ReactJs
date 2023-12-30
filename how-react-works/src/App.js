4378c483acafcb6a79206777d151a97aa3cdcc4d
Karuna sagar <karunasagar129130@gmail.com>
2023-12-30 21:52:48 +0530

    Add undo button to tab content

--- how-react-works/src/App.js
+++ how-react-works/src/App.js
@@ -61,7 +61,10 @@ function Tab({ num, activeTab, onClick }) {
 function TabContent({ item }) {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
-
+  function handleUndo() {
+    setShowDetails(true)
+    setLikes(0)
+  }
   function handleInc() {
     setLikes(likes + 1);
   }
@@ -84,7 +87,7 @@ function TabContent({ item }) {
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
+        <button onClick={handleUndo}>Undo</button>
         <button>Undo in 2s</button>
       </div>
     </div>

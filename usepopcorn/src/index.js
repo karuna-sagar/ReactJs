d7a1069ff6bf76345bc4efb82cc26f035e84cbae
Karuna sagar <karunasagar129130@gmail.com>
2023-12-30 22:49:14 +0530

    Update: Remove StarRating component, add App component

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
+import './index.css';
 import App from './App';
-import StarRating from './StarRating';
+// import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StarRating messages={['Terrible', "Bad", "Okay", "Good", "Amazing"]} />
-    {/* <App /> */}
+    {/* <StarRating messages={['Terrible', "Bad", "Okay", "Good", "Amazing"]} /> */}
+    <App />
   </React.StrictMode>
 );

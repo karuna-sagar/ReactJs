6ea848bfc34f0cabdb9e0aae6426286e4273b998
Karuna sagar <karunasagar129130@gmail.com>
2024-01-05 23:40:29 +0530

    Update currency converter to disable inputs while fetching data

--- currency_converter/src/App.js
+++ currency_converter/src/App.js
@@ -5,24 +5,27 @@ export default function App() {
   const [fromCurr, setFromCurr] = useState("INR");
   const [toCurr, setToCurr] = useState("USD");
   const [converted, setConverted] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
   useEffect(function () {
     async function convert() {
+      setIsLoading(true)
       const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurr}&to=${toCurr}`)
       const data = await res.json()
       setConverted(data.rates[toCurr])
+      setIsLoading(false)
     }
     convert()
-  }, [])
+  }, [amount, fromCurr, toCurr])
   return (
     <div>
       <input type="text" value={amount} onChange={e => setAmount(Number(e.target.value))} />
-      <select value={fromCurr} onChange={e => setFromCurr(e.target.value)} >
+      <select value={fromCurr} onChange={e => setFromCurr(e.target.value)} disabled={isLoading} >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select value={toCurr} onChange={e => setToCurr(e.target.value)} >
+      <select value={toCurr} onChange={e => setToCurr(e.target.value)} disabled={isLoading} >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>

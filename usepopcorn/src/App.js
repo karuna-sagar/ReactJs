0a94cf109b83890ad899fd7dbd224f83495e0bc2
Karuna sagar <karunasagar129130@gmail.com>
2024-01-05 22:26:51 +0530

    Add event listener for Escape key to close movie details modal

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -286,12 +286,17 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onCloseMovie();
   }
   useEffect(function () {
-    document.addEventListener('keydown', function (e) {
+    function callback(e) {
       if (e.code === "Escape") {
         onCloseMovie();
       }
-    })
-  }, [])
+    }
+    document.addEventListener('keydown', callback)
+
+    return function () {
+      document.removeEventListener('keydown', callback)
+    }
+  }, [onCloseMovie])
   useEffect(function () {
     if (!title) return;
     document.title = `Movie | ${title}`;

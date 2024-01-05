9e48691f8da70a90440166c3e03ea8525f9f7cc6
Karuna sagar <karunasagar129130@gmail.com>
2024-01-05 23:24:22 +0530

    Add state variables and event handlers for the currency converter app.

--- currency_converter/src/App.js
+++ currency_converter/src/App.js
@@ -1,15 +1,22 @@
+import { useEffect, useState } from "react";
 import "./style.css";
 export default function App() {
+  const [amount, setAmount] = useState(100);
+  const [fromCurr, setFromCurr] = useState("INR");
+  const [toCurr, setToCurr] = useState("USD");
+  useEffect(function () {
+
+  }, [])
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input type="text" value={amount} onChange={e => setAmount(Number(e.target.value))} />
+      <select value={fromCurr} onChange={e => setFromCurr(e.target.value)} >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select value={toCurr} onChange={e => setToCurr(e.target.value)} >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>

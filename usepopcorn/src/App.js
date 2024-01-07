d82abd3b392cf0ecbcb8d898ece341fed5e47f45
Karuna sagar <karunasagar129130@gmail.com>
2024-01-07 22:22:42 +0530

    Add local storage for watched movies

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -8,10 +8,14 @@ const average = (arr) =>
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("")
   const [selectedId, setSelectedId] = useState(null)
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const storeValue = localStorage.getItem('watched');
+    return JSON.parse(storeValue)
+  });
   function handleSlectMovie(id) {
     setSelectedId(selectedId => id === selectedId ? null : id);
   }
@@ -22,13 +26,16 @@ export default function App() {
 
   function handleAddWatched(movie) {
     setWatched(watched => [...watched, movie])
-    localStorage.setItem("watched", JSON.stringify([...watched, movie]))
+
   }
 
   function handleDeleteWatched(id) {
     setWatched(watched.filter(movies => movies.imdbID !== id))
   }
 
+  useEffect(function () {
+    localStorage.setItem("watched", JSON.stringify(watched))
+  }, [watched])
 
   useEffect(function () {
     const controller = new AbortController();
@@ -132,7 +139,7 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
-
+  const inputEl = useRef(null);
   return (
     <input
       className="search"
@@ -140,6 +147,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }

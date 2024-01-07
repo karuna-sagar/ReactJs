f18d12ebf27185279f762c1ab59a73713dc5888a
Karuna sagar <karunasagar129130@gmail.com>
2024-01-07 22:41:09 +0530

    Add event listener to document to detect Enter key press and clear search input when pressed.

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -140,8 +140,21 @@ function Logo() {
 
 function Search({ query, setQuery }) {
   const inputEl = useRef(null);
+
   useEffect(function () {
-    inputEl.current.focus()
+
+
+    function callback(e) {
+      if (document.activeElement === inputEl.current) {
+        return;
+      }
+      if (e.code === 'Enter') {
+        inputEl.current.focus();
+        setQuery('');
+      }
+    }
+    document.addEventListener('keydown', callback);
+    return () => document.addEventListener('keydown', callback)
   }, [])
   return (
     <input

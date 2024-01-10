7b4557b38cc4cae1c3de6c024f1d259ce00228e4
Karuna sagar <karunasagar129130@gmail.com>
2024-01-10 12:43:50 +0530

    Remove WatchedMoviesList component and WatchedMovie component.

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -382,13 +382,3 @@ function WatchedMovie({ movie, onDeleteWAtched }) {
     </li>
   );
 }
-
-function WatchedMoviesList({ watched, onDeleteWAtched }) {
-  return (
-    <ul className="list">
-      {watched.map((movie) => (
-        <WatchedMovie movie={movie} key={movie.imdbID} onDeleteWAtched={onDeleteWAtched} />
-      ))}
-    </ul>
-  );
-}

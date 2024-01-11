777b57fe2217e3b5932462574029c7cd05ce5300
Karuna sagar <karunasagar129130@gmail.com>
2024-01-11 23:17:10 +0530

    Update DateCounter component to use a reducer instead of useState

--- react-quiz/src/DateCounter 1.js
+++ react-quiz/src/DateCounter 1.js
@@ -19,37 +19,46 @@ function reducer(state, action) {
 
 }
 function DateCounter() {
-
+  // const [count, setCount] = useState(0);
+  // const initialState = { count: 0, step: 1 }
   const [state, dispatch] = useReducer(reducer, initialState)
   const { count, step } = state
+  // const [count, dispatch] = useReducer(reducer, 0);
 
+  // const [step, setStep] = useState(1);
+
+  // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
     dispatch({ type: "dec" });
-
+    // dispatch({ type: "dec", payload: -1 });
+    // setCount((count) => count - 1);
+    // setCount((count) => count - step);
   };
 
   const inc = function () {
-
+    // dispatch({ type: "inc", payload: 1 });
     dispatch({ type: "inc" });
-
+    // setCount((count) => count + 1);
+    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-
+    // setCount(Number(e.target.value));
     dispatch({ type: "setCount", payload: Number(e.target.value) })
   };
 
   const defineStep = function (e) {
-
+    // setStep(Number(e.target.value));
     dispatch({ type: "setStep", payload: Number(e.target.value) })
   };
 
   const reset = function () {
     dispatch({ type: "reset" })
-
+    // setCount(0);
+    // setStep(1);
   };
 
   return (

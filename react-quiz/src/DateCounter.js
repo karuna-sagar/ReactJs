077850b038f43498d81c5d8159a9214d3614d97e
Karuna sagar <karunasagar129130@gmail.com>
2024-01-11 20:34:49 +0530

    Update DateCounter component to use a reducer for state management.

--- react-quiz/src/DateCounter.js
+++ react-quiz/src/DateCounter.js
@@ -2,7 +2,9 @@ import { useReducer, useState } from "react";
 
 function reducer(state, action) {
   console.log(state, action)
-  return state + action;
+  if (action.type === "inc") return state + action.payload
+  if (action.type === "dec") return state - action.payload
+  if (action.type === "setCount") return action.payload
 }
 function DateCounter() {
   // const [count, setCount] = useState(0);
@@ -15,19 +17,20 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch(-1)
+    dispatch({ type: "dec", payload: -1 });
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
 
   const inc = function () {
-    dispatch(1)
+    dispatch({ type: "inc", payload: 1 });
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
     // setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) })
   };
 
   const defineStep = function (e) {

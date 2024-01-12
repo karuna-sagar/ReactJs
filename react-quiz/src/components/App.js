7bf52fd1ee8e19c8d0a857112634a03ac747da9e
Karuna sagar <karunasagar129130@gmail.com>
2024-01-12 19:02:28 +0530

    Add state management and fetch questions from the backend.

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -5,11 +5,25 @@ import Header from "./Header";
 import Main from "./Main";
 
 export default function App() {
-
+  const initialState = {
+    questions: [],
+    // 'Loading' ,'ready','error' ,'active','finished'
+    status: 'loading',
+  }
+  const [state, dispatch] = useReducer(reducer, initialState);
+  function reducer(state, action) {
+    console.log(state)
+    switch (action.type) {
+      case "dataReceived":
+        return { ...state, questions: action.payload, status: 'ready' }
+      default:
+        throw new Error('Unknown action')
+    }
+  }
   useEffect(function () {
     fetch("http://localhost:9000/questions")
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => dispatch({ type: "dataReceived", payload: data }))
       .catch(err => console.log(err))
   }, [])
 

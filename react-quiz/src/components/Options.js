3e6b0cffbd871ad01b836ae24a81866e4a2d47a6
Karuna sagar <karunasagar129130@gmail.com>
2024-01-12 20:43:30 +0530

    Update Options component to highlight correct and wrong answers (generated by blackbox.ai )

--- react-quiz/src/components/Options.js
+++ react-quiz/src/components/Options.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export default function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null
   return (
     <div className="options">
-      {question.options.map((option, index) => <button className='btn btn-option' key={option} onClick={() => dispatch({ type: "newAnswer", payload: index })} >{option}</button>)}
-    </div>
+      {question.options.map((option, index) => <button className={`btn btn-option ${index === answer ? 'answer' : ""}  ${hasAnswered ? index === question.correctOption ? 'correct' : 'wrong' : ""}`} key={option} onClick={() => dispatch({ type: "newAnswer", payload: index })} disabled={hasAnswered} >{option}</button>)
+      }
+    </div >
   )
 }

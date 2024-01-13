be87a5694acd54207241a0af1cf069d8791832f1
Karuna sagar <karunasagar129130@gmail.com>
2024-01-13 21:03:54 +0530

    Add minutes to timer display

--- react-quiz/src/components/Timer.js
+++ react-quiz/src/components/Timer.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 
 export default function Timer({ dispatch, secondsRemaining }) {
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
   useEffect(
     function () {
       const id = setInterval(function () {
@@ -10,5 +12,9 @@ export default function Timer({ dispatch, secondsRemaining }) {
     },
     [dispatch]
   );
-  return <div className="timer">{secondsRemaining}</div>;
+  return (
+    <div className="timer">
+      {mins < 10 && "0"} {mins}: {seconds < 10 && "0"} {seconds}
+    </div>
+  );
 }

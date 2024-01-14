a994ad5db0a87c0de5b0893e5d16b59cb58daada
Karuna sagar <karunasagar129130@gmail.com>
2024-01-15 00:40:24 +0530

    Update dependencies and add React Router

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,26 @@
 // import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
 export default function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="product"
+          element={<Product />}
+        />
+
+        <Route
+          path="pricing"
+          element={<Pricing />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }

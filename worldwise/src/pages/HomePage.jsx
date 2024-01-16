f4ec0a935431095c3b9690863827c849ab03efca
Karuna sagar <karunasagar129130@gmail.com>
2024-01-17 02:33:07 +0530

    Add link to Pricing page

--- worldwise/src/pages/HomePage.jsx
+++ worldwise/src/pages/HomePage.jsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom";
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
 }

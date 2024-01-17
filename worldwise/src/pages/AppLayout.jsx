1d9029cad23429f6d5c599587398c3533a0e35d4
Karuna sagar <karunasagar129130@gmail.com>
2024-01-17 13:15:26 +0530

    Add Map component and import it in AppLayout

--- worldwise/src/pages/AppLayout.jsx
+++ worldwise/src/pages/AppLayout.jsx
@@ -1,9 +1,11 @@
+import Map from "../Components/Map";
 import SideBar from "../Components/SideBar";
 import styles from "./AppLayout.module.css";
 export default function AppLayout() {
   return (
     <div className={styles.app}>
       <SideBar />
+      <Map />
     </div>
   );
 }

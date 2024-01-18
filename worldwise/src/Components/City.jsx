1e4601978cf44761fa83e7a410410ba9d939a3bd
Karuna sagar <karunasagar129130@gmail.com>
2024-01-19 00:56:32 +0530

    Add latitude and longitude parameters to the URL and display them on the City and Map components.

--- worldwise/src/Components/City.jsx
+++ worldwise/src/Components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 function City() {
@@ -10,11 +10,19 @@ function City() {
     date: "2027-10-31T15:59:59.138Z",
     notes: "My favorite city so far!",
   };
+  const [searchParams, setSearchParam] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
   const { cityName, emoji, date, notes } = currentCity;
 
   return (
-    <h1>City {id}</h1>
+    <>
+      <h1>City {id}</h1>
+      <p>
+        Position : {lat},{lng}
+      </p>
+    </>
     // <div className={styles.city}>
     //   <div className={styles.row}>
     //     <h6>City name</h6>
--- worldwise/src/Components/Map.jsx
+++ worldwise/src/Components/Map.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import styles from "./Map.module.css";
+import { useSearchParams } from "react-router-dom";
+
 export default function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParam] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      <h1>
+        Position : {lat} , {lng}
+      </h1>
+    </div>
+  );
 }

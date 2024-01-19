a517255974e4038cbe36bff04cea13e2bc120d03
Karuna sagar <karunasagar129130@gmail.com>
2024-01-20 00:33:50 +0530

    Update City component to use CitiesContext and fetch city data on mount

--- worldwise/src/Contexts/CitiesContext.jsx
+++ worldwise/src/Contexts/CitiesContext.jsx
@@ -4,7 +4,7 @@ const CitiesContext = createContext();
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState();
+  const [currentCity, setCurrentCity] = useState({});
   useEffect(function () {
     async function fetchCities() {
       try {

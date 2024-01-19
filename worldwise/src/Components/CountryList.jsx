396f17e23ce9b4ad09dbbe9f9d446f74ac243a40
Karuna sagar <karunasagar129130@gmail.com>
2024-01-19 23:45:46 +0530

    Update CountryList component to use CitiesContext

--- worldwise/src/Components/CountryList.jsx
+++ worldwise/src/Components/CountryList.jsx
@@ -3,7 +3,9 @@ import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
 import CountryItem from "./CountryItem";
 import Message from "./Message";
-export default function CountryList({ cities, isLoading }) {
+import { useCities } from "../Contexts/CitiesContext";
+export default function CountryList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return <Message message="Add your first city by clicking on a city" />;

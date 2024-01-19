b3aa1fefb24c2b9848133fb899768bca43df930b
Karuna sagar <karunasagar129130@gmail.com>
2024-01-19 23:54:26 +0530

    Update getCity function to fetch city by id

--- worldwise/src/Contexts/CitiesContext.jsx
+++ worldwise/src/Contexts/CitiesContext.jsx
@@ -24,7 +24,7 @@ function CitiesProvider({ children }) {
   async function getCity(id) {
     try {
       setIsLoading(true);
-      const res = await fetch(`${BASE_URL}/cities`);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
       const data = await res.json();
       setCurrentCity(data);
     } catch {

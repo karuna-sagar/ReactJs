d92deaa6a25af139ceade77ed9a08f8734f33510
Karuna sagar <karunasagar129130@gmail.com>
2024-01-20 10:47:26 +0530

    map position changes

--- worldwise/src/Components/Map.jsx
+++ worldwise/src/Components/Map.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import styles from "./Map.module.css";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import { useCities } from "../Contexts/CitiesContext";
 
 export default function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities();
   const [searchParams, setSearchParam] = useSearchParams();
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const lat = searchParams.get("lat");
@@ -24,11 +26,16 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );

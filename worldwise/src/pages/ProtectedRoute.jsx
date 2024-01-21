75256d6f543ed30f068a8a9022326abb999ca6dd
Karuna sagar <karunasagar129130@gmail.com>
2024-01-21 18:53:37 +0530

    PRotected Router

---
+++ worldwise/src/pages/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useAuth } from "../Contexts/fakeAuthContext";
+import { useNavigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+  useEffect(
+    function () {
+      if (!isAuthenticated) navigate("/");
+    },
+    [isAuthenticated, navigate]
+  );
+  return children;
+}
+export { ProtectedRoute };

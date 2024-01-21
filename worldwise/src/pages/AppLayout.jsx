d2f5d409403135e2c103146536c08086a1171213
Karuna sagar <karunasagar129130@gmail.com>
2024-01-21 12:09:49 +0530

    Add User component and logout functionality

--- worldwise/src/pages/AppLayout.jsx
+++ worldwise/src/pages/AppLayout.jsx
@@ -1,11 +1,13 @@
 import Map from "../Components/Map";
 import SideBar from "../Components/SideBar";
+import User from "../Components/User";
 import styles from "./AppLayout.module.css";
 export default function AppLayout() {
   return (
     <div className={styles.app}>
       <SideBar />
       <Map />
+      <User />
     </div>
   );
 }

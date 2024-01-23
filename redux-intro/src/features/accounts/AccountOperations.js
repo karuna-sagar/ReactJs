be031b7422cfef59dabe974661fb263a446d750e
Karuna sagar <karunasagar129130@gmail.com>
2024-01-23 17:53:55 +0530

    Update account operations component to use new account store structure.

--- redux-intro/src/features/accounts/AccountOperations.js
+++ redux-intro/src/features/accounts/AccountOperations.js
@@ -8,8 +8,12 @@ function AccountOperations() {
   const [loanPurpose, setLoanPurpose] = useState("");
   const [currency, setCurrency] = useState("USD");
   const dispatch = useDispatch();
-  const account = useSelector((store) => store.account); //ye mainly account se ata
-  console.log(account);
+  const {
+    loan: currentLoan,
+    loanPurpose: currentLoanPurpose,
+    balance,
+  } = useSelector((store) => store.account); //ye mainly account se ata
+  console.log(balance);
   function handleDeposit() {
     if (!depositAmount) return;
     dispatch(deposit(depositAmount));
@@ -17,7 +21,7 @@ function AccountOperations() {
   }
 
   function handleWithdrawal() {
-    if (withdrawalAmount > account.balance) return setWithdrawalAmount("");
+    if (withdrawalAmount > balance) return setWithdrawalAmount("");
     dispatch(withdraw(withdrawalAmount));
     setWithdrawalAmount("");
   }
@@ -29,9 +33,7 @@ function AccountOperations() {
     setLoanPurpose("");
   }
 
-  function handlePayLoan() {
-    if()
-  }
+  function handlePayLoan() {}
 
   return (
     <div>
@@ -85,7 +87,9 @@ function AccountOperations() {
         </div>
 
         <div>
-          <span>Pay back $X</span>
+          <span>
+            Pay back ${currentLoan} {currentLoanPurpose}
+          </span>
           <button onClick={handlePayLoan}>Pay loan</button>
         </div>
       </div>

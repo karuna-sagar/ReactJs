ae16546e598a9b6d6f95661d3463d36798773599
Karuna sagar <karunasagar129130@gmail.com>
2024-01-24 13:21:44 +0530

    Changed the import statement for the `thunk` middleware from `import thunk from "redux-thunk";` to `import { thunk } from "redux-thunk";`

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 import customerReducer from "./features/customers/customerSlice";
 import accountReducer from "./features/accounts/accountSlice";
 const rootReducer = combineReducers({

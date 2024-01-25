c823b6b8e4463917b50b45d618e710681f154c82
Karuna sagar <karunasagar129130@gmail.com>
2024-01-25 16:12:09 +0530

    Add redux-devtools-extension to package.json and package-lock.json

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import customerReducer from "./features/customers/customerSlice";
 import accountReducer from "./features/accounts/accountSlice";
 const rootReducer = combineReducers({

ffc6959e8d5d58c2052100153672f78a883bfb57
Karuna sagar <karunasagar129130@gmail.com>
2024-01-25 17:04:17 +0530

    Update dependencies and use `redux-toolkit`

--- redux-intro/src/features/accounts/accountSlice.js
+++ redux-intro/src/features/accounts/accountSlice.js
@@ -1,71 +1,24 @@
-const initialStatesAccount = {
+import { autoBatchEnhancer, createSlice } from "@reduxjs/toolkit";
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
-export default function accountReducer(state = initialStatesAccount, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return {
-        ...state,
-        balance: state.balance + action.payload,
-        isLoading: false,
-      };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      // LATER
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: action.payload.amount + state.balance,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-    case "account/convertingCurrency":
-      return {
-        ...state,
-        isLoading: true,
-      };
-    default:
-      return state;
-  }
-}
-export function deposit(amount, currency) {
-  if (currency === "USD") return { type: "account/deposit", payload: amount };
-  return async function (dispatch, getState) {
-    dispatch({ type: "account/convertingCurrency" });
-    const res = await fetch(
-      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
-    );
-    const data = await res.json();
-    const converted = data.rates.USD;
-    dispatch({ type: "account/deposit", payload: converted });
-  };
-}
 
-export function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-export function requestLoan(amount, purpose) {
-  return {
-    type: "account/requestLoan",
-    payload: {
-      amount: 1000,
-      purpose: "Buy a new Car",
+const accountSlice = createSlice({
+  name:'account',
+  initialState,
+  reducers:{
+    deposit(state,action){
+      state.balance = state.balance + action.payload
     },
-  };
-}
-export function payLoan() {
-  return {
-    type: "account/payLoan",
-  };
-}
+    withdraw(state,action){
+      state.balance = state.balance - action.payload;
+    },
+    requestLoan(state,action){
+      if(state.loan > 0) return 
+      state.loan = 
+    }
+  }
+})

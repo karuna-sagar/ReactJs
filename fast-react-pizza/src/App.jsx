49bdcf97ce644be33e5af92b91932e1aa512ea3d
Karuna sagar <karunasagar129130@gmail.com>
2024-01-26 13:39:53 +0530

    Update dependencies and add React Route

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,3 +1,30 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Order from "./features/order/Order";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+  { path: "/cart", element: <Cart /> },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+]);
+
 export default function App() {
-  return <div>Hello Vite</div>;
+  return <RouterProvider router={router} />;
 }

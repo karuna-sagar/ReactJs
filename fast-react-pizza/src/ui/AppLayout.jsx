882911f65c199a7f29aae607eb42ffc0e93dd136
Karuna sagar <karunasagar129130@gmail.com>
2024-01-26 21:07:09 +0530

    Added useNavigate and Loader to Menu and AppLayout components.

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
 export default function AppLayout() {
+  const navigation = useNavigate();
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />

4340c24677d6ce4e1d2d7d8d5bffc57d754b84a4
Karuna sagar <karunasagar129130@gmail.com>
2024-01-26 17:54:48 +0530

    Replace the placeholder text with the Outlet component

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
 
@@ -6,7 +7,7 @@ export default function AppLayout() {
     <div>
       <Header />
       <main>
-        <h1>Sagar</h1>
+        <Outlet />
       </main>
       <CartOverview />
     </div>

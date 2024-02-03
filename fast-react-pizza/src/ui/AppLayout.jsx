2661285efbbbd38c7502af30611ced4b9cdba5f4
Karuna sagar <karunasagar129130@gmail.com>
2024-02-03 17:03:14 +0530

    ui added

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,20 +1,27 @@
-import { Outlet, useNavigation } from "react-router-dom";
-import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
 import Loader from "./Loader";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
 
-export default function AppLayout() {
+function AppLayout() {
   const navigation = useNavigation();
-  // console.log(navigation);
   const isLoading = navigation.state === "loading";
+
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
+
       <Header />
-      <main>
-        <Outlet />
-      </main>
+
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+
       <CartOverview />
     </div>
   );
 }
+
+export default AppLayout;
--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 import Username from "../features/user/Username";
-export default function Header() {
+
+function Header() {
   return (
-    <header className="bg-yellow-500 px-4 py-3 uppercase border-b border-stone-200">
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link
         to="/"
         className="tracking-widest"
       >
-        Fast React Pizza co.
+        Fast React Pizza Co.
       </Link>
 
       <SearchOrder />
@@ -16,3 +17,5 @@ export default function Header() {
     </header>
   );
 }
+
+export default Header;

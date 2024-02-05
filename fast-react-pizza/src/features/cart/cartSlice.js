e378a1c44f52883e459e654d6d86c07832b18d7b
Karuna sagar <karunasagar129130@gmail.com>
2024-02-05 23:40:14 +0530

    create a cartslice and make initial and redux element

---
+++ fast-react-pizza/src/features/cart/cartSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  cart: [
+    {
+      pizzaId: 12,
+      name: "Paneer Pizza",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+};
+const cartSlice = createSlice({});

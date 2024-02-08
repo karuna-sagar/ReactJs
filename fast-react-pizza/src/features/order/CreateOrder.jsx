e29d7e42440705140fd4bf255925cfb4f6f35b05
Karuna sagar <karunasagar129130@gmail.com>
2024-02-08 16:18:16 +0530

    Update CreateOrder component and userSlice to handle address errors

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -14,29 +14,29 @@ const isValidPhone = (str) =>
     str
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+// const fakeCart = [
+//   {
+//     pizzaId: 12,
+//     name: "Mediterranean",
+//     quantity: 2,
+//     unitPrice: 16,
+//     totalPrice: 32,
+//   },
+//   {
+//     pizzaId: 6,
+//     name: "Vegetale",
+//     quantity: 1,
+//     unitPrice: 13,
+//     totalPrice: 13,
+//   },
+//   {
+//     pizzaId: 11,
+//     name: "Spinach and Mushroom",
+//     quantity: 1,
+//     unitPrice: 15,
+//     totalPrice: 15,
+//   },
+// ];
 
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
@@ -49,8 +49,9 @@ function CreateOrder() {
     status: addressStatus,
     position,
     address,
-  } = useSelector((state) => state.user.user);
-  const isLoading = addressStatus === "loading";
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+  const isLoadingAddress = addressStatus === "loading";
   // const cart = fakeCart;
   const cart = useSelector(getCart);
   const totalCartPrice = useSelector(getTotalCartPrice);
@@ -99,15 +100,20 @@ function CreateOrder() {
               className="input w-full"
               type="text"
               name="address"
-              disabled={isLoading}
+              disabled={isLoadingAddress}
               defaultValue={address}
               required
             />
+            {addressStatus === "error" && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {errorAddress}
+              </p>
+            )}
           </div>
           {!position.latitude && !position.longitude && (
-            <span className="absolute right-[3px] z-50">
+            <span className="absolute right-[3px] top-[3px] z-50 md:right-[5px] md:top-[5px]">
               <Button
-                disabled={isLoading}
+                disabled={isLoadingAddress}
                 type="small"
                 onClick={(e) => {
                   e.preventDefault();
@@ -143,8 +149,17 @@ function CreateOrder() {
             name="cart"
             value={JSON.stringify(cart)}
           />
+          <input
+            type="hidden"
+            name="position"
+            value={
+              position.latitude && position.longitude
+                ? `${position.latitude},${position.longitude}`
+                : ""
+            }
+          />
           <Button
-            disabled={isSubmitting}
+            disabled={isSubmitting || isLoadingAddress}
             type="primary"
           >
             {isSubmitting
@@ -175,7 +190,7 @@ export async function action({ request }) {
   if (Object.keys(errors).length > 0) return errors;
 
   // If everything is okay, create new order and redirect
-
+  console.log(order);
   const newOrder = await createOrder(order);
 
   store.dispatch(clearItem());

90222217ff974846e6adce686c71b406b61970c7
Karuna sagar <karunasagar129130@gmail.com>
2024-02-08 16:45:02 +0530

    Update OrderItem component to display ingredients if available, otherwise display "Loading..."

--- fast-react-pizza/src/features/order/OrderItem.jsx
+++ fast-react-pizza/src/features/order/OrderItem.jsx
@@ -10,8 +10,10 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
           <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p className="font-bold">{formatCurrency(totalPrice)}</p>
-        <p className="text-sm capitalize italic text-stone-500"> </p>
       </div>
+      <p className="text-sm capitalize italic text-stone-500">
+        {isLoadingIngredients ? "Loading... " : ingredients.join(", ")}
+      </p>
     </li>
   );
 }

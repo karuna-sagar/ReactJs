cdce35fa90474c3132380f816e58f2020fa1d5af
Karuna sagar <karunasagar129130@gmail.com>
2024-02-13 20:13:38 +0530

    Update homepage layout

--- gym-website/src/pages/Home.js
+++ gym-website/src/pages/Home.js
@@ -1,10 +1,16 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import HeroBanner from '../components/HeroBanner'
+import SearchExercises from '../components/SearchExercises'
+import Excercises from '../components/Excercises'
 
 function Home() {
     return (
-        <div>
-            Home
-        </div>
+        <Box>
+            <HeroBanner />
+            <SearchExercises />
+            <Excercises />
+        </Box>
     )
 }
 

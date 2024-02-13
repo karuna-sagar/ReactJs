1dd3c97a38fdd78f5878908ee1c7c6f3fb838838
Karuna sagar <karunasagar129130@gmail.com>
2024-02-13 19:32:58 +0530

    Add React Router to enable routing and navigation between pages.

--- gym-website/src/App.js
+++ gym-website/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material'
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <Box width="400px" >
+      NavBar
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExcerciseDetail />} />
+      </Routes>
+    </Box>
   )
 }
 

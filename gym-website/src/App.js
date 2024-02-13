00c26a914887df6a4141346c8b30c1fcd088a262
Karuna sagar <karunasagar129130@gmail.com>
2024-02-13 19:41:43 +0530

    Add NavBar component and update routes

--- gym-website/src/App.js
+++ gym-website/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material'
+import NavBar from './components/NavBar'
+import ExcerciseDetail from './pages/ExcerciseDetail'
+import Home from './pages/Home'
 function App() {
   return (
     <Box width="400px" >
-      NavBar
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExcerciseDetail />} />

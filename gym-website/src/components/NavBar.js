c6c5d32ac72091976ef907dbfc05d73401abf2df
Karuna sagar <karunasagar129130@gmail.com>
2024-02-13 20:10:18 +0530

    Changed the navigation bar layout and styles

--- gym-website/src/components/NavBar.js
+++ gym-website/src/components/NavBar.js
@@ -4,11 +4,15 @@ import { Stack } from '@mui/material'
 import Logo from '../assets/images/Logo.png'
 const NavBar = () => {
     return (
-        <Stack>
+        <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
             <Link to="/">
                 <img src={Logo} alt="Logo" style={{ width: '48px', height: '72px', margin: "0 2px" }} />
             </Link>
-            <Stack>
+            <Stack direction="row"
+                gap="40px"
+                fontFamily="Alegreya"
+                fontSize="24px"
+                alignItems="flex-end">
                 <Link to="/" style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }} >Home</Link>
                 <a href="#exercise" style={{ textDecoration: 'none', color: '#3A1212' }}>Exercise</a>
             </Stack>

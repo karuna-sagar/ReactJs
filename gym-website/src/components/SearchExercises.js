05e5655b89b16459b2bb029d55f50c9f1708c963
Karuna sagar <karunasagar129130@gmail.com>
2024-02-15 02:32:11 +0530

    Added search functionality to the exercise search page.

--- gym-website/src/components/SearchExercises.js
+++ gym-website/src/components/SearchExercises.js
@@ -1,7 +1,14 @@
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
+import { fetchData, exerciseOptions } from '../utils/fetchData';
 
 const SearchExercises = () => {
+    const [search, setSearch] = useState('');
+    const handleSearch = async () => {
+        if (search) {
+            const exercisesData = await fetchData()
+        }
+    }
     return (
         <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
             <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
@@ -11,11 +18,12 @@ const SearchExercises = () => {
                 <TextField
                     height="76px"
                     sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
-
+                    value={search}
                     placeholder="Search Exercises"
                     type="text"
+                    onChange={e => setSearch(e.target.value.toLowerCase())}
                 />
-                <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }}>
+                <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
                     Search
                 </Button>
             </Box>

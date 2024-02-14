0fa92a8e2b06a5f839798fd103ac49abb8d4c63d
Karuna sagar <karunasagar129130@gmail.com>
2024-02-14 20:46:10 +0530

    Update HeroBanner and SearchExercises components

--- gym-website/src/App.js
+++ gym-website/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material'
+import './App.css';
 import NavBar from './components/NavBar'
 import ExcerciseDetail from './pages/ExcerciseDetail'
 import Home from './pages/Home'

7976d3f66059e9b1075b37f0bf20fde2a3180f54
Karuna sagar <karunasagar129130@gmail.com>
2024-02-14 19:55:47 +0530

    Add hero banner text

--- gym-website/src/components/HeroBanner.js
+++ gym-website/src/components/HeroBanner.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import { Stack, Box, Typography } from '@mui/material'
 const HeroBanner = () => {
     return (
-        <div>
-
-        </div>
+        <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px" >
+            <Typography color="#FF2625" fontWeight="600" fontSize="600" >
+                Fitness Club
+            </Typography>
+            <Typography>
+                Sweat,Smile <br /> and  Repeat
+            </Typography>
+            <Typography>
+                Check out the most effective exercises<br /> and
+            </Typography>
+        </Box>
     )
 }
 

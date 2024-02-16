dc514228727f04c4c6da02d67dcc96117e1b4c6d
Karuna sagar <karunasagar129130@gmail.com>
2024-02-17 02:30:11 +0530

    Update components, styling, and add functionality

--- gym-website/src/utils/fetchData.js
+++ gym-website/src/utils/fetchData.js
@@ -8,7 +8,13 @@ export const exerciseOptions = {
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
 };
-
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '79758a5f1dmsh4e5aca16fbf268bp1db990jsn0d0cedf67fe8',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+};
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options)
     const data = await response.json();

91e5f9cf3e1feb522d86120fdbfcbd0d390a3802
Karuna sagar <karunasagar129130@gmail.com>
2024-02-19 02:03:41 +0530

    Refactored codebase to use Header component

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './components/header/Header'
 
 const App = () => {
   return (
     <div>
-      App
+      <Header />
     </div>
   )
 }
--- e-commerce/src/components/header/header.js
+++ e-commerce/src/components/header/header.js
@@ -1,11 +1,20 @@
 import React from 'react'
-
-const header = () => {
+import './Header.css'
+import Logo from '../../assets/images/logo.svg'
+const Header = () => {
     return (
         <div>
-            Header
+            <header>
+                <div className='container-fluid'>
+                    <div className="row">
+                        <div className="col-sm-2">
+                            <img src={Logo} alt="Logo" />
+                        </div>
+                    </div>
+                </div>
+            </header>
         </div>
     )
 }
 
-export default header
+export default Header

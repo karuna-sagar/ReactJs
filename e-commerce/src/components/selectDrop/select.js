5b9fa82b65e2964afd42ea84d7da539683e99588
Karuna sagar <karunasagar129130@gmail.com>
2024-02-21 03:15:06 +0530

    Added a list of items to the dropdown menu in the select component.

--- e-commerce/src/components/selectDrop/select.js
+++ e-commerce/src/components/selectDrop/select.js
@@ -7,6 +7,22 @@ const Select = () => {
                 <div className="searchField">
                     <input type="text" />
                 </div>
+                <ul>
+                    <li>Milks and Dairies</li>
+                    <li>Wines & Drinks</li>
+                    <li>Clothing & beauty</li>
+                    <li>Fresh Seafood</li>
+                    <li>Pet Foods & Toy</li>
+                    <li>Fast food</li>
+                    <li>Baking material</li>
+                    <li>Vegetables</li>
+                    <li>Fresh Fruit</li>
+                    <li>Bread and Juice</li>
+                    <li>Milks and Dairies</li>
+                    <li>Wines & Drinks</li>
+                    <li>Clothing & beauty</li>
+                    <li>Fresh Seafood</li>
+                </ul>
             </div>
         </>
     )

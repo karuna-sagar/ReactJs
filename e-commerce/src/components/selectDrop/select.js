9e8620c7d11016be6a69d75e27c9cc0c57d1f4c2
Karuna sagar <karunasagar129130@gmail.com>
2024-02-24 03:05:15 +0530

    Update select component to use state to manage open/closed state and selected index

--- e-commerce/src/components/selectDrop/select.js
+++ e-commerce/src/components/selectDrop/select.js
@@ -2,8 +2,13 @@ import React, { useState } from 'react'
 import '../selectDrop/select.css'
 const Select = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [isIndex, setIsIndex] = useState(0);
     function openSelect() {
         setIsOpen(!isOpen)
+    }
+    function closeSelect(index) {
+        setIsIndex(index)
+        setIsOpen(false)
     }
     return (
 
@@ -17,20 +22,21 @@ const Select = () => {
                             <input type="text" />
                         </div>
                         <ul className='searchResults'>
-                            <li onClick={() => setIsOpen(false)} >Milks and Dairies</li>
-                            <li onClick={() => setIsOpen(false)} >Wines & Drinks</li>
-                            <li onClick={() => setIsOpen(false)} >Clothing & beauty</li>
-                            <li onClick={() => setIsOpen(false)} >Fresh Seafood</li>
-                            <li onClick={() => setIsOpen(false)} >Pet Foods & Toy</li>
-                            <li onClick={() => setIsOpen(false)} >Fast food</li>
-                            <li onClick={() => setIsOpen(false)} >Baking material</li>
-                            <li onClick={() => setIsOpen(false)} >Vegetables</li>
-                            <li onClick={() => setIsOpen(false)} >Fresh Fruit</li>
-                            <li onClick={() => setIsOpen(false)} >Bread and Juice</li>
-                            <li onClick={() => setIsOpen(false)} >Milks and Dairies</li>
-                            <li onClick={() => setIsOpen(false)} >Wines & Drinks</li>
-                            <li onClick={() => setIsOpen(false)} >Clothing & beauty</li>
-                            <li onClick={() => setIsOpen(false)} >Fresh Seafood</li>
+                            <li onClick={() => closeSelect(0)} className={`${isIndex === 0 ? 'active' : ""}`} >All Categories</li>
+                            <li onClick={() => closeSelect(1)} className={`${isIndex === 1 ? 'active' : ""}`} >Milks and Dairies</li>
+                            <li onClick={() => closeSelect(2)} className={`${isIndex === 2 ? 'active' : ""}`} >Wines & Drinks</li>
+                            <li onClick={() => closeSelect(3)} className={`${isIndex === 3 ? 'active' : ""}`} >Clothing & beauty</li>
+                            <li onClick={() => closeSelect(4)} className={`${isIndex === 4 ? 'active' : ""}`} >Fresh Seafood</li>
+                            <li onClick={() => closeSelect(5)} className={`${isIndex === 5 ? 'active' : ""}`} >Pet Foods & Toy</li>
+                            <li onClick={() => closeSelect(6)} className={`${isIndex === 6 ? 'active' : ""}`} >Fast food</li>
+                            <li onClick={() => closeSelect(7)} className={`${isIndex === 7 ? 'active' : ""}`} >Baking material</li>
+                            <li onClick={() => closeSelect(8)} className={`${isIndex === 8 ? 'active' : ""}`} >Vegetables</li>
+                            <li onClick={() => closeSelect(9)} className={`${isIndex === 9 ? 'active' : ""}`} >Fresh Fruit</li>
+                            <li onClick={() => closeSelect(10)} className={`${isIndex === 10 ? 'active' : ""}`} >Bread and Juice</li>
+                            <li onClick={() => closeSelect(11)} className={`${isIndex === 11 ? 'active' : ""}`} >Milks and Dairies</li>
+                            <li onClick={() => closeSelect(12)} className={`${isIndex === 12 ? 'active' : ""}`} >Wines & Drinks</li>
+                            <li onClick={() => closeSelect(13)} className={`${isIndex === 13 ? 'active' : ""}`} >Clothing & beauty</li>
+                            <li onClick={() => closeSelect(14)} className={`${isIndex === 14 ? 'active' : ""}`} >Fresh Seafood</li>
                         </ul>
                     </div>
                 }

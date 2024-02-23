90499f7a584162a087023be9d2e1d3e09b4ac5cf
Karuna sagar <karunasagar129130@gmail.com>
2024-02-24 02:41:32 +0530

    Update select drop component to use React hooks and add open/close functionality

--- e-commerce/src/components/selectDrop/select.js
+++ e-commerce/src/components/selectDrop/select.js
@@ -1,32 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../selectDrop/select.css'
 const Select = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    function openSelect() {
+        setIsOpen(!isOpen)
+    }
     return (
+
         <>
             <div className="selectDropWrapper cursor position-relative">
-                <span className='openSelect' >All Categories</span>
-                <div className="selectDrop">
-                    <div className="searchField">
-                        <input type="text" />
-                    </div>
-                    <ul className='searchResults'>
-                        <li>Milks and Dairies</li>
-                        <li>Wines & Drinks</li>
-                        <li>Clothing & beauty</li>
-                        <li>Fresh Seafood</li>
-                        <li>Pet Foods & Toy</li>
-                        <li>Fast food</li>
-                        <li>Baking material</li>
-                        <li>Vegetables</li>
-                        <li>Fresh Fruit</li>
-                        <li>Bread and Juice</li>
-                        <li>Milks and Dairies</li>
-                        <li>Wines & Drinks</li>
-                        <li>Clothing & beauty</li>
-                        <li>Fresh Seafood</li>
-                    </ul>
-                </div>
+                <span className='openSelect' onClick={openSelect} >All Categories</span>
 
+                {isOpen === true &&
+                    <div className="selectDrop">
+                        <div className="searchField">
+                            <input type="text" />
+                        </div>
+                        <ul className='searchResults'>
+                            <li>Milks and Dairies</li>
+                            <li>Wines & Drinks</li>
+                            <li>Clothing & beauty</li>
+                            <li>Fresh Seafood</li>
+                            <li>Pet Foods & Toy</li>
+                            <li>Fast food</li>
+                            <li>Baking material</li>
+                            <li>Vegetables</li>
+                            <li>Fresh Fruit</li>
+                            <li>Bread and Juice</li>
+                            <li>Milks and Dairies</li>
+                            <li>Wines & Drinks</li>
+                            <li>Clothing & beauty</li>
+                            <li>Fresh Seafood</li>
+                        </ul>
+                    </div>
+                }
             </div>
 
         </>

6b4eded3c4b494e8d16c0861263bfc49f629e4f6
Karuna sagar <karunasagar129130@gmail.com>
2024-02-25 14:54:21 +0530

    Refactor ExpenseItem component

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   const expenses = [
     {
@@ -21,15 +22,30 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  return (
-    <div className="App">
-      <h1>
-        <ExpenseItem id={expenses[0].id} title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-        <ExpenseItem id={expenses[1].id} title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-        <ExpenseItem id={expenses[2].id} title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-        <ExpenseItem id={expenses[3].id} title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
 
-      </h1>
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }

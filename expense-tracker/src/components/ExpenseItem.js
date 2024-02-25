8eed7bd7bc2e7d362e4b8fc971f2659f03308245
Karuna sagar <karunasagar129130@gmail.com>
2024-02-25 13:49:16 +0530

    Add ExpenseItem component and display expense items

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
     return (
-        <div>
-            hello ji sir ji
+        <div className='expense-item'>
+            <div>{props.date.toISOString()}</div>
         </div>
     )
 }

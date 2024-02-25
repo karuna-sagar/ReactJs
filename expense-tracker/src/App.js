fd88e15851055e3c8427a9aa51a5194873319ddc
Karuna sagar <karunasagar129130@gmail.com>
2024-02-26 00:07:51 +0530

    Refactor code: - Remove unused components and files related to `New Expense` form. - Update `App.js` to use the new `Expenses` component)

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,5 +1,6 @@
-import ExpenseItem from './components/Expenses/ExpenseItem';
-import NewExpense from './components/NewExpens/NewExpense';
+
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpenses/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -27,26 +28,8 @@ const App = () => {
   return (
     <div>
       <NewExpense />
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses items={expenses} />
+
     </div>
   );
 }
---
+++ expense-tracker/src/components/NewExpenses/NewExpenseForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import './NewExpenseForm.css'
+const NewExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('')
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value)
+    }
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+    }
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+    }
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        console.log(expenseData);
+    }
+    return (
+        <form onSubmit={submitHandler}>
+            <div className='new-expense__controls'>
+                <div className="new-expense__control" onChange={titleChangeHandler}>
+                    <label>Title</label>
+                    <input type="text" />
+                </div>
+                <div className="new-expense__control" onChange={amountChangeHandler}>
+                    <label>Amount</label>
+                    <input type="text" min="0.01" max="0.01" />
+                </div>
+                <div className="new-expense__control" onChange={dateChangeHandler}>
+                    <label>Date</label>
+                    <input type="date" min="2019-01-01" max="2022-12-31" />
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type='submit'>Add Expense</button>
+            </div>
+        </form>
+    )
+}
+
+export default NewExpenseForm

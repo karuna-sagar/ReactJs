6c71a891aae793d26101580facc45c611e3f9bf7
Karuna sagar <karunasagar129130@gmail.com>
2024-02-27 23:13:59 +0530

    Changed the fallback UI when there are no expenses to display from a `<p>` to an `<h2>` with the text "Found No Message"

--- expense-tracker/src/components/Expenses/ExpensesList.js
+++ expense-tracker/src/components/Expenses/ExpensesList.js
@@ -3,18 +3,21 @@ import ExpenseItem from './ExpenseItem';
 
 const ExpensesList = (props) => {
 
-    let expensesContent = <p>No expenses found.</p>;
-
-    if (props.items.length > 0) {
-        expensesContent = props.items.map((expense) => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-        ));
-    }
+    if (props.items.length === 0) { return <h2 className='expenses-list__fallback'>Found No Message</h2> }
+    return (
+        <ul className='expenses-list'>
+            {
+                props.items.map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))
+            }
+        </ul>
+    )
 
 }
 

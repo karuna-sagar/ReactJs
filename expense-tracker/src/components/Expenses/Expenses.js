a48d4d30f2c70a42ad7fea2ca2f6f50109d38ae1
Karuna sagar <karunasagar129130@gmail.com>
2024-02-27 19:45:51 +0530

    Update App.js and Expenses.js to use state and filter expenses by year.

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -11,12 +11,20 @@ const Expenses = (props) => {
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear);
     };
-
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear
+    })
     return (
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                <ExpenseItem
+                {props.items.map(expense => <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />)}
+                {/* <ExpenseItem
                     title={props.items[0].title}
                     amount={props.items[0].amount}
                     date={props.items[0].date}
@@ -35,7 +43,7 @@ const Expenses = (props) => {
                     title={props.items[3].title}
                     amount={props.items[3].amount}
                     date={props.items[3].date}
-                />
+                /> */}
             </Card>
         </div>
     );

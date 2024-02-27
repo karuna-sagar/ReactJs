008546d71a4660bca08442a55ad72e820284a688
Karuna sagar <karunasagar129130@gmail.com>
2024-02-28 01:16:58 +0530

    chart are added to the ui

--- expense-tracker/src/components/Chart/Chart.js
+++ expense-tracker/src/components/Chart/Chart.js
@@ -2,9 +2,11 @@ import React from 'react'
 import ChartBar from './ChartBar'
 import './Chart.css'
 const Chart = (props) => {
+    const dataPointValue = props.dataPoints.map(dataPoint => dataPoint.value)
+    const totalMaximum = Math.max(...dataPointValue)
     return (
         <div className="chart">
-            {props.dataPoints.map(dataPoint => <ChartBar id={dataPoint.label} value={dataPoint.value} maxValue={null} label={dataPoint.label} />)}
+            {props.dataPoints.map(dataPoint => <ChartBar id={dataPoint.label} value={dataPoint.value} maxValue={totalMaximum} label={dataPoint.label} />)}
         </div>
     )
 }

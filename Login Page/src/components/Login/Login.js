22992a478da80ed815eea94161772ab064912965
Karuna sagar <karunasagar129130@gmail.com>
2024-03-12 23:17:48 +0530

    Remove form validation logic from email and password change handlers.

--- Login Page/src/components/Login/Login.js
+++ Login Page/src/components/Login/Login.js
@@ -25,17 +25,17 @@ const Login = (props) => {
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+    // setFormIsValid(
+    //   event.target.value.includes('@') && enteredPassword.trim().length > 6
+    // );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
+    // setFormIsValid(
+    //   event.target.value.trim().length > 6 && enteredEmail.includes('@')
+    // );
   };
 
   const validateEmailHandler = () => {

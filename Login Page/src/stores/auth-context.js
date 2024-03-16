5b78181be075a49482dd8cdc0f9bb03b861c61f1
Karuna sagar <karunasagar129130@gmail.com>
2024-03-17 02:22:49 +0530

    Added login and logout handlers to the AuthContextProvider.

--- Login Page/src/stores/auth-context.js
+++ Login Page/src/stores/auth-context.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => { },
+    onLogin: (email, password) => { }
 })
+const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const loginHandler = (email, password) => {
+        setIsLoggedIn(true);
+    };
 
+    const logoutHandler = () => {
+        setIsLoggedIn(false);
+    };
+    return (
+        <AuthContext.Provider value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler }} >
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
 export default AuthContext

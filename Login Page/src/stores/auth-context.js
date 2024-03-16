0f5f42b369a81d7953c02b526e095e1fa44e3bc2
Karuna sagar <karunasagar129130@gmail.com>
2024-03-17 02:26:45 +0530

    Add local storage to track login state

--- Login Page/src/stores/auth-context.js
+++ Login Page/src/stores/auth-context.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const AuthContext = React.createContext({
     isLoggedIn: false,
@@ -8,12 +8,20 @@ const AuthContext = React.createContext({
 const AuthContextProvider = (props) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const loginHandler = (email, password) => {
+        localStorage.setItem('isLoggedIn', '1')
         setIsLoggedIn(true);
     };
 
     const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn')
         setIsLoggedIn(false);
     };
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+        if (storedUserLoggedInInformation === '1') {
+            setIsLoggedIn(true)
+        }
+    }, [])
     return (
         <AuthContext.Provider value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler }} >
             {props.children}

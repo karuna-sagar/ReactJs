bd7c01b515629c04fc73925a21d2c5e1d0f82c22
Karuna sagar <karunasagar129130@gmail.com>
2024-03-20 03:27:24 +0530

    Remove the HeaderCartButton component and the associated CSS.

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function HandleCartButton() {
+function HeaderCartButton() {
   return (
     <div>
 
@@ -8,4 +8,4 @@ function HandleCartButton() {
   )
 }
 
-export default HandleCartButton
+export default HeaderCartButton

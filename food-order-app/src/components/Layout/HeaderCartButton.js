d87e686cdbcee78c5ce78154cebe6b190d6c96a7
Karuna sagar <karunasagar129130@gmail.com>
2024-03-21 02:54:22 +0530

    Changed the import statement for the CSS module from './HeaderCartButton' to './HeaderCartButton.module.css'.

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import CartIcon from '../Cart/CartIcon'
-import classes from './HeaderCartButton'
+import classes from './HeaderCartButton.module.css '
 function HeaderCartButton() {
   return (
     <button className={classes.button} >

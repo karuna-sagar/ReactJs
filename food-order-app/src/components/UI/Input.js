1064c4a57bc8aba74e800153f3d61bf233c2ae73
Karuna sagar <karunasagar129130@gmail.com>
2024-03-22 03:03:25 +0530

    "Use Input component in MealItemForm and pass props; update Input component to accept props and render input element with them"
    Explanation:
    
    * The `MealItemForm` component has been updated to use the `Input` component from `UI/Input` instead of a regular input element. It passes an object as the `input` prop to the `Input` component.
    * The `Input` component has been updated to accept a `props` object, which includes a nested `input` object. It now renders the input element using the spread operator (`{...props.input}`) to apply the properties passed in the `input` object. Additionally, the `Input` component's wrapper div class has been changed from `classes.form` to `classes.input`.

--- food-order-app/src/components/UI/Input.js
+++ food-order-app/src/components/UI/Input.js
@@ -2,9 +2,9 @@ import React from 'react'
 import classes from './Input.module.css'
 const Input = (props) => {
     return (
-        <div className={classes.form}>
+        <div className={classes.input}>
             <label htmlFor={props.input.id}>{props.label}</label>
-            <input>{...props.input}</input>
+            <input {...props.input} />
         </div>
     )
 }

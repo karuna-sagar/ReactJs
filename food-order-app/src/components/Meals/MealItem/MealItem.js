a865e7f4b54dd695cda9c34143c3bcb3626b40b1
Karuna sagar <karunasagar129130@gmail.com>
2024-03-22 02:00:53 +0530

    "Import classes from MealItem.module.css and use it to style MealItem component"

--- food-order-app/src/components/Meals/MealItem/MealItem.js
+++ food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import c
+import classes from './MealItem.module.css'
 const MealItem = (props) => {
     const price = `$${props.price.toFixed(2)}`;
     return (
-        <li>
+        <li className={classes.meal}>
             <div>
                 <h3>{props.name}</h3>
                 <div>{props.description}</div>

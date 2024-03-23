ca527f2858ef2cfa465321f6d9c4783d384d85fd
Karuna sagar <karunasagar129130@gmail.com>
2024-03-23 12:10:57 +0530

    "Added onClick prop to Close button in Cart component and passed it to Modal's onClose prop; updated Modal and ModalOverlay to use functional components with correct JSX syntax; updated Backdrop to use correct JSX syntax"

--- food-order-app/src/components/UI/Modal.js
+++ food-order-app/src/components/UI/Modal.js
@@ -1,22 +1,32 @@
-import classes from './Modal.module.css'
-import React, { Fragment } from 'react'
-import ReactDOM from 'react-dom'
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+import classes from './Modal.module.css';
+
 const Backdrop = (props) => {
-    return <div className={classes.backdrop} onClick={props.onClose} />
-}
+    return <div className={classes.backdrop} onClick={props.onClose} />;
+};
+
 const ModalOverlay = (props) => {
-    return <div className={classes.modal}>
-        <div className="classes content">{props.children}</div>
-    </div>
-}
-const portalElement = document.getElementById('overlays')
+    return (
+        <div className={classes.modal}>
+            <div className={classes.content}>{props.children}</div>
+        </div>
+    );
+};
+
+const portalElement = document.getElementById('overlays');
+
 const Modal = (props) => {
     return (
         <Fragment>
-            {ReactDOM.createPortal(<Backdrop onClick={props.onClose} />, portalElement)}
-            {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+            {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+            {ReactDOM.createPortal(
+                <ModalOverlay>{props.children}</ModalOverlay>,
+                portalElement
+            )}
         </Fragment>
-    )
-}
+    );
+};
 
-export default Modal
+export default Modal;

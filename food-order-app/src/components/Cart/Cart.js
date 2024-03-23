972b3d391a83bc254a4a3680f44e94fa96572176
Karuna sagar <karunasagar129130@gmail.com>
2024-03-24 01:13:01 +0530

    "Updated Cart and CartProvider components to include add/remove item handlers and reducer for removing items"

--- food-order-app/src/components/Cart/Cart.js
+++ food-order-app/src/components/Cart/Cart.js
@@ -8,8 +8,8 @@ const Cart = (props) => {
     const cartCtx = useContext(CartContext)
     const hasItems = cartCtx.items.length > 0
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
-    const cartItemAddHandler = (item) => { }
-    const cartItemRemoveHandler = id => { }
+    const cartItemAddHandler = (item) => { cartCtx.addItem({ ...item, amount: 1 }) }
+    const cartItemRemoveHandler = id => { cartCtx.removeItem(id) }
     const cartItem = <ul className={classes['cart-items']}>{cartCtx.items.map(item => <CartItem key={item.id} name={item.name} price={item.price} amount={item.amount} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)} />)
     }</ul>
     return (

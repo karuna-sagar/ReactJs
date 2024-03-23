4f47ccbb91432b0edca4aabbdc034c97e31d819a
Karuna sagar <karunasagar129130@gmail.com>
2024-03-23 17:24:48 +0530

    forward ref is introduced

--- food-order-app/src/components/UI/Input.js
+++ food-order-app/src/components/UI/Input.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import classes from './Input.module.css'
-const Input = React.createRef((props) => {
+const Input = React.createRef((props, ref) => {
     return (
         <div className={classes.input}>
             <label htmlFor={props.input.id}>{props.label}</label>
-            <input {...props.input} />
+            <input ref={ref} {...props.input} />
         </div>
     )
 })

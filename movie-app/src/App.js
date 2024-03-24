385d4057445ce66bfdf0527d94378acfddbc1c54
Karuna sagar <karunasagar129130@gmail.com>
2024-03-25 01:04:29 +0530

    "Add error handling and null check to movie fetching function"
    This diff adds error handling to the fetchMoviesHandler function using a try-catch block. It also initializes an error state using the useState hook and sets the error message in case of a fetch error. Additionally, it initializes the error state to null.

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -6,20 +6,31 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   async function fetchMoviesHandler() {
     setIsLoading(true)
-    const response = await fetch('https://swapi.dev/api/films/')
-    const data = await response.json();
-
-    const transformedMovies = data.results.map(movieData => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date
+    try {
+      const response = await fetch('https://swapi.dev/api/films/')
+      if (!response.ok) {
+        throw new Error("Something wen wring");
       }
-    })
-    setMovies(transformedMovies);
+      const data = await response.json();
+
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      })
+      setMovies(transformedMovies);
+
+    }
+    catch (error) {
+      setError(error.message);
+
+    }
     setIsLoading(false)
   }
 

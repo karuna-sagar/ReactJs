ee0a1817599a90f4174026566052fdbb50e62714
Karuna sagar <karunasagar129130@gmail.com>
2024-04-03 21:31:24 +0530

    "Add optional properties to useHttp hook and use it in App component"
    This commit includes changes to the `useHttp` hook in `hooks/use-http.js`, where optional properties are added to the `requestConfig` object for method, headers, and body. In addition, the `App.js` file has been updated to use the modified `useHttp` hook and a new `transformTask` function has been added to process the fetched data.

--- Small React Project/Challenge1/src/App.js
+++ Small React Project/Challenge1/src/App.js
@@ -2,18 +2,27 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from '../hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
+  const transformTask = tasksObj => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  }
+  useHttp({ url: 'https://react-app-995c0-default-rtdb.firebaseio.com/tasks.json' }, transformTask)
 
   const fetchTasks = async (taskText) => {
     setIsLoading(true);
     setError(null);
     try {
       const response = await fetch(
-        'https://react-app-995c0-default-rtdb.firebaseio.com/tasks.json'
+
       );
 
       if (!response.ok) {
@@ -22,13 +31,7 @@ function App() {
 
       const data = await response.json();
 
-      const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
     } catch (err) {
       setError(err.message || 'Something went wrong!');
     }

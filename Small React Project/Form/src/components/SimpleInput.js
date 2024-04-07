6e2b4c54587d572521c892ae693e606ceaed93c6
Karuna sagar <karunasagar129130@gmail.com>
2024-04-08 00:06:20 +0530

    "Added useRef for name input and ref forwarding; cleared input value after submission"
    Explanation:
    
    * The code diff imports `useRef` from React and adds a new state variable `nameInputRef` that uses it.
    * The `SimpleInput` component now receives a `ref` prop, which is forwarded to the `nameInputRef` using the `ref` attribute on the input element.
    * The `value` attribute is added to the input element and set to `enteredName`.
    * The `submissionHandler` now gets the current value of the input element using `nameInputRef.current.value` and logs it to the console. It also clears the input value by setting `nameInputRef.current.value` to an empty string.
    * The `enteredName` state is also cleared after submission by calling `setEnteredName('')`.

--- Small React Project/Form/src/components/SimpleInput.js
+++ Small React Project/Form/src/components/SimpleInput.js
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('')
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
@@ -7,12 +8,16 @@ const SimpleInput = (props) => {
   const submissionHandler = event => {
     event.preventDefault();
     console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue)
+    setEnteredName('');
+    nameInputRef.current.value = '';
   }
   return (
     <form onSubmit={submissionHandler} >
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameInputChangeHandler} />
+        <input type='text' id='name' onChange={nameInputChangeHandler} ref={nameInputRef} value={enteredName} />
       </div>
       <div className="form-actions">
         <button>Submit</button>

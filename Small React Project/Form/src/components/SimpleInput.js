8826290a99bc648efab7c52202a22afed9670d74
Karuna sagar <karunasagar129130@gmail.com>
2024-04-08 02:00:26 +0530

    "Added state for input validity and error message for empty name in SimpleInput component"
    The diff includes changes to the SimpleInput component in a React project. It adds a new state variable called enteredNameIsValid and a conditional error message that is displayed when the entered name is empty. The code also includes a fix for a minor bug in the if condition for checking the entered name.

--- Small React Project/Form/src/components/SimpleInput.js
+++ Small React Project/Form/src/components/SimpleInput.js
@@ -2,14 +2,17 @@ import { useRef, useState } from 'react'
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('')
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
   }
   const submissionHandler = event => {
     event.preventDefault();
-    if (enteredName.trim === '') {
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
       return
     }
+    setEnteredNameIsValid(true);
     console.log(enteredName);
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue)
@@ -21,6 +24,7 @@ const SimpleInput = (props) => {
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
         <input type='text' id='name' onChange={nameInputChangeHandler} ref={nameInputRef} value={enteredName} />
+        {!enteredNameIsValid && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>

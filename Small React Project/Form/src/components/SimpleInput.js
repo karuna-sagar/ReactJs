4e42312a4a78841b98f5fef45cdfcc90aa796cdc
Karuna sagar <karunasagar129130@gmail.com>
2024-04-07 23:08:50 +0530

    "Added local state and event handlers to SimpleInput component"
    Explanation:
    This code diff shows the addition of local state and event handlers to the SimpleInput component in a React application. The `useState` hook from React has been imported and used to create a state variable `enteredName` and its corresponding state updater function `setEnteredName`. Two new event handlers, `nameInputChangeHandler` and `submissionHandler`, have also been added to handle changes in the input field and form submission respectively. The input field in the component now has an `onChange` prop that is set to `nameInputChangeHandler` to update the state with the user's input. The form also has an `onSubmit` prop that is set to `submissionHandler` to prevent the default form submission behavior.

--- Small React Project/Form/src/components/SimpleInput.js
+++ Small React Project/Form/src/components/SimpleInput.js
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('')
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  }
+  const submissionHandler = event => {
+    event.preventDefault();
+  }
   return (
-    <form>
+    <form onSubmit={submissionHandler} >
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>

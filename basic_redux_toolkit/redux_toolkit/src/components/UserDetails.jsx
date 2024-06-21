36dd62094043fc206b6f0a95a578845817f2fb63
Karuna sagar <karunasagar129130@gmail.com>
2024-06-22 03:04:01 +0530

    "Added UserDetails component with basic UI structure"

---
+++ basic_redux_toolkit/redux_toolkit/src/components/UserDetails.jsx
@@ -0,0 +1,25 @@
+
+
+function UserDetails() {
+    return (
+        <>
+            <div className='content'>
+                <div className='admin-table'>
+                    <div className="admin-subtitle">
+                        List Of User Details
+                    </div>
+                    <button>Add New Users</button>
+                </div>
+                <ul>
+                    <li>hi</li>
+                    <li>hii</li>
+                </ul>
+                <hr />
+
+            </div>
+
+        </>
+    )
+}
+
+export default UserDetails

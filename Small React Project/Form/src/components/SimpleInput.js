18e4f455ec382f46aa5c29cefdfa5d1f29140136
Karuna sagar <karunasagar129130@gmail.com>
2024-06-22 01:46:54 +0530

    "Removed useRef, added onBlur handler, and updated input validation logic in SimpleInput component"

--- Small React Project/Form/src/components/SimpleInput.js
+++ Small React Project/Form/src/components/SimpleInput.js
@@ -1,11 +1,20 @@
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  
   const [enteredName, setEnteredName] = useState('')
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+  const enteredNameIsValid = enteredName.trim() !=='';
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
+    if (event.target.value.trim() !== '') {
+      setEnteredNameIsValid(true);
+    }
+  }
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouched(true)
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+    }
   }
   const submissionHandler = event => {
     event.preventDefault();
@@ -27,7 +36,7 @@ const SimpleInput = (props) => {
     <form onSubmit={submissionHandler} >
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameInputChangeHandler} ref={nameInputRef} value={enteredName} />
+        <input type='text' id='name' onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} value={enteredName} />
         {nameInputIsValid && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className="form-actions">

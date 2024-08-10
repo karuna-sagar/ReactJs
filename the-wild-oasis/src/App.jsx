c7b098babe80a18cfa1ec7b33ddb56eac1b8e470
Karuna sagar <karunasagar129130@gmail.com>
2024-08-10 10:14:56 +0530

    "Updated index.html with new meta tags and font imports, refactored App.jsx to use new UI components, and created separate files for Button and Input components."

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,41 +1,30 @@
 import styled from 'styled-components'
+import GlboalStyles from './styles/GlobalStyles'
+import BUTTON from './ui/Button'
+import Input from './ui/Input'
+import Heading from './ui/Heading'
 
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`
 
-const BUTTON = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight:  500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  margin: 20px;
-`
-const Input = styled.button`
-  border : 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`
+
+
+
 const StyledApp = styled.main`
   background-color: orangered;
   padding: 20px;
 `
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <BUTTON onClick={() => alert('click in')}>Check in</BUTTON>
-      <BUTTON onClick={() => alert('check out')}>Check Out</BUTTON>
-      <Input type='number' placeholder="Number of guests" />
-      <Input type='number' placeholder="Number of guests" />
-    </StyledApp>
-
+    <>
+      <GlboalStyles />
+      <StyledApp>
+        <Heading>The Wild Oasis</Heading>
+        <BUTTON onClick={() => alert('click in')}>Check in</BUTTON>
+        <BUTTON onClick={() => alert('check out')}>Check Out</BUTTON>
+        <Input type='number' placeholder="Number of guests" />
+        <Input type='number' placeholder="Number of guests" />
+      </StyledApp>
+    </>
   )
 }
 

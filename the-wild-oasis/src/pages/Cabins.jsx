5c8ac183244f94d9f08029ba65a5afbf34158ff1
Karuna sagar <karunasagar129130@gmail.com>
2024-08-10 23:26:37 +0530

    Added an img tag to the Cabins component with a specific src attribute.

--- the-wild-oasis/src/pages/Cabins.jsx
+++ the-wild-oasis/src/pages/Cabins.jsx
@@ -12,6 +12,7 @@ function Cabins() {
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://drigeelhjneikhucopma.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2024-08-10T17%3A16%3A26.524Z" />
     </Row>
   );
 }

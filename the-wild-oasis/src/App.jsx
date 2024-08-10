a3d02a8122164333eb045c1205aa8ba848901aef
Karuna sagar <karunasagar129130@gmail.com>
2024-08-10 09:37:12 +0530

    global styles are added by using styled component

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,10 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import styled from 'styled-components'
 
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`
+
+const BUTTON = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight:  500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  margin: 20px;
+`
+const Input = styled.button`
+  border : 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`
 function App() {
   return (
-    <div>Hello World</div>
+    <StyledApp>
+      <H1>The Wild Oasis</H1>
+      <BUTTON onClick={() => alert('click in')}>Check in</BUTTON>
+      <BUTTON onClick={() => alert('check out')}>Check Out</BUTTON>
+      <Input type='number' placeholder="Number of guests" />
+      <Input type='number' placeholder="Number of guests" />
+    </StyledApp>
+
   )
 }
 

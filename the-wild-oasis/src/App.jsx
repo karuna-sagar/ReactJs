eebfeda8e51d8796391e510497512329945cc4b6
Karuna sagar <karunasagar129130@gmail.com>
2024-08-11 10:33:38 +0530

    "Added React Query and devtools, updated App.jsx to use QueryClientProvider, created new CabinTable component, and updated Cabins.jsx to use CabinTable"

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import GlobalStyles from "./styles/GlobalStyles"
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
@@ -11,9 +12,20 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound"
 import AppLayout from "./pages/AppLayout";
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  }
+})
 function App() {
   return (
-    <>
+
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -30,8 +42,8 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+    </QueryClientProvider>
 
-    </>
   )
 }
 export default App
--- the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -23,3 +23,12 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+import React from 'react'
+
+function CabinTable() {
+  return (
+    <div>Table</div>
+  )
+}
+
+export default CabinTable

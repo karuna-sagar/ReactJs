4c4bc10c22fc51eab2906a98a6a7890306beb4d6
Karuna sagar <karunasagar129130@gmail.com>
2024-09-22 01:27:50 +0530

    "Added filtering and sorting to useBookings hook and getBookings API function"

--- the-wild-oasis/src/services/apiBookings.js
+++ the-wild-oasis/src/services/apiBookings.js
@@ -2,8 +2,10 @@ import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
 
-export async function getBookings() {
-  const { data, error } = await supabase.from("bookings").select("id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name), guests(fullName,email) ")
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase.from("bookings").select("id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice,cabins(name), guests(fullName,email) ")
+  if (filter !== null) query = query.eq(filter.field, filter.value)
+  const { data, error } = await query;
   if (error) {
     console.log(error)
     throw new Error("Bookings could not be loaded");

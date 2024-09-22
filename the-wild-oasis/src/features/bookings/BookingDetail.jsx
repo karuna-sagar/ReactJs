7b19d547e55aed32e42119bf49f9feac13cf13da
Karuna sagar <karunasagar129130@gmail.com>
2024-09-22 20:01:52 +0530

    "Refactor BookingDetail component, update Booking page to use BookingDetail, and modify apiBookings service to return only data"

--- the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import { useNavigate } from 'react-router-dom';
 import BookingDataBox from "./BookingDataBox";
 import Row from "../../ui/Row";
 import Heading from "../../ui/Heading";
@@ -8,7 +8,9 @@ import ButtonGroup from "../../ui/ButtonGroup";
 import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 
+import { useBooking } from "./useBooking";
 import { useMoveBack } from "../../hooks/useMoveBack";
+import Spinner from "../../ui/Spinner";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -17,10 +19,12 @@ const HeadingGroup = styled.div`
 `;
 
 function BookingDetail() {
-  const booking = {};
-  const status = "checked-in";
-
+  // const navigate = useNavigate();
   const moveBack = useMoveBack();
+  const { booking, isLoading } = useBooking();
+  if (isLoading) return <Spinner />
+  const { status, id: bookingId } = booking
+
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -32,7 +36,7 @@ function BookingDetail() {
     <>
       <Row type="horizontal">
         <HeadingGroup>
-          <Heading as="h1">Booking #X</Heading>
+          <Heading as="h1">Booking #{bookingId}</Heading>
           <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
         </HeadingGroup>
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>

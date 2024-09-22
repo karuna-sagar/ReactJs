59c9770204ead70a7c548e6b2f53011cdbf8644a
Karuna sagar <karunasagar129130@gmail.com>
2024-09-22 15:30:13 +0530

    "Added pagination and sorting to bookings feature, updated useBookings and apiBookings to handle pagination and sorting, and created a PAGE_SIZE constant"

--- the-wild-oasis/src/services/apiBookings.js
+++ the-wild-oasis/src/services/apiBookings.js
@@ -1,8 +1,9 @@
+import { PAGE_SIZE } from "../utils/constants";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
 
   let query = supabase
     .from("bookings")
@@ -12,6 +13,11 @@ export async function getBookings({ filter, sortBy }) {
     );
   if (filter) query = query.eq(filter.field, filter.value)
   if (sortBy) query = query.order(sortBy.field, { ascending: sortBy.direction === "asc" })
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE
+    const to = from + PAGE_SIZE - 1
+    query = query.range(from, to)
+  }
   const { data, error, count } = await query;
   if (error) {
     console.log(error)

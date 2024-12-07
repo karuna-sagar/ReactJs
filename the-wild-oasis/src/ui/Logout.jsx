85e3ecf0bf702684c8aa1034340d5c851f04e667
Karuna sagar <karunasagar129130@gmail.com>
2024-12-07 23:23:09 +0530

    `Added logout function to apiAuth.js and added newline at end of file`

---
+++ the-wild-oasis/src/ui/Logout.jsx
@@ -0,0 +1,10 @@
+import ButtonIcon from "./ButtonIcon";
+import {HiArrowRightOnRectangle} from "react-icons/hi2"
+function Logout(){
+ return(
+    <ButtonIcon>
+        <HiArrowRightOnRectangle />
+    </ButtonIcon>
+ );
+}
+export default Logout;

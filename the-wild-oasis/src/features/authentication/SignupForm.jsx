71e591bff941df7302ab6fbcced85426f3570925
Karuna sagar <karunasagar129130@gmail.com>
2024-12-09 02:10:59 +0530

    `Added disabled attribute to form fields when isLoading is true`

--- the-wild-oasis/src/features/authentication/SignupForm.jsx
+++ the-wild-oasis/src/features/authentication/SignupForm.jsx
@@ -18,30 +18,31 @@ function SignupForm() {
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow label="Full name" error={errors?.fullName?.message}>
-        <Input type="text" id="fullName" {...register("fullName", {required: "This field is required "})} />
+        <Input type="text" id="fullName" 
+        disabled={isLoading} {...register("fullName", {required: "This field is required "})} />
       </FormRow> 
 
       <FormRow label="Email address" error={errors?.email?.message}>
-        <Input type="email" id="email" {...register("email", {required: "This field is required ", pattern:{
+        <Input type="email" id="email"  disabled={isLoading} {...register("email", {required: "This field is required ", pattern:{
           value: /\S+@\S+\.\S+/,
           message: "Please Provide valid email address"
         }})} />
       </FormRow>
    
       <FormRow label="Password (min 8 characters)" error={errors?.password?.message}>
-        <Input type="password" id="password" {...register("password", {required: "This field is required ", minLength:{
+        <Input type="password" id="password" disabled={isLoading} {...register("password", {required: "This field is required ", minLength:{
           value:8,
           message: "Password must be at least 8 characters"
         }})}/>
       </FormRow>
 
       <FormRow label="Repeat password" error={errors?.passwordConfirm?.message}>
-        <Input type="password" id="passwordConfirm" {...register("passwordConfirm", {required: "This field is required ", validate: (value) => value === getValues().password || 'Password needs to match'})}/>
+        <Input type="password" id="passwordConfirm" disabled={isLoading} {...register("passwordConfirm", {required: "This field is required ", validate: (value) => value === getValues().password || 'Password needs to match'})}/>
       </FormRow>
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button disabled={isLoading} variation="secondary" type="reset">
           Cancel
         </Button>
         <Button>Create new user</Button>

73ae9f5cec9e24dc6ca814343cace4a091224686
Karuna sagar <karunasagar129130@gmail.com>
2024-12-11 00:16:39 +0530

    `feat: Update user data form with new API and UX improvements`

--- the-wild-oasis/src/services/apiAuth.js
+++ the-wild-oasis/src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -58,7 +58,7 @@ export async function updateCurrentUser({ password, fullName, avatar }) {
   // 3. Update avatar in the user
   const { data: updatedUser, error: error2 } = supabase.auth.updateUser({
     data: {
-      avatar: `{supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
     },
   });
   if (error2) throw new Error(error2.message);

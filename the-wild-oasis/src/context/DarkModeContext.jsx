7ba4d668b642e3abf5519baaa8abc5266f3fa602
Karuna sagar <karunasagar129130@gmail.com>
2024-12-13 00:42:45 +0530

    `feat: Added DarkModeProvider and updated routes`

---
+++ the-wild-oasis/src/context/DarkModeContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useCallback, useContext } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+  function toggleDarkMode() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+  return (
+    <DarkModeContext.Provider value={isDarkMode}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+  if (context === undefined)
+    throw new Error("DarkModeContext was used outside of DarkModeProvider");
+  return context;
+}
+export { DarkModeProvider, useDarkMode };
--- the-wild-oasis/src/ui/DarkModeToggle.jsx
+++ the-wild-oasis/src/ui/DarkModeToggle.jsx
@@ -1,6 +1,8 @@
+import { useDarkMode } from "../context/DarkModeContext";
 import ButtonIcon from "./ButtonIcon";
 import { HiOutlineMoon } from "react-icons/hi2";
 function DarkModeToggle() {
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
   return (
     <ButtonIcon>
       <HiOutlineMoon />

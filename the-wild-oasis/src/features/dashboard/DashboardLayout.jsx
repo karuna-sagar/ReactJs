fb97fb9531b365035884865b983200d897b43301
Karuna sagar <karunasagar129130@gmail.com>
2024-12-15 17:08:29 +0530

    `Update TodayActivity component and import it in DashboardLayout`

--- the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import { useCabins } from "../cabins/useCabins";
 import Stats from "./Stats";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -28,7 +29,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Today Activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart
         bookings={bookings}

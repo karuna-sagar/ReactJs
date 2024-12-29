9f9b0dc9a6fabd8af8ae471753b4f56851eb25d4
Karuna sagar <karunasagar129130@gmail.com>
2024-12-29 13:36:35 +0530

    `Added player information and login section to the App component`

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <h1>tic-tac-toe</h1>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        game board
+      </div>
+      log in
+    </main>
+  );
 }
 
 export default App;

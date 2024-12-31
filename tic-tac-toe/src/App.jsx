c64ad50b63d27e0f6b5296970e067b2f8f511f54
Karuna sagar <karunasagar129130@gmail.com>
2024-12-31 18:00:23 +0530

    `Added player name change functionality and restart button`

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -17,10 +17,14 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 function App() {
+  const [players, setPlayers] = useState({
+    X: "Player 1",
+    O: "Player 2",
+  });
   // const [activePlayer, setActivePlayer] = useState("X");
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurns);
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map((array) => [...array])];
   for (const turn of gameTurns) {
     const { square, player } = turn;
     const { row, col } = square;
@@ -54,6 +58,17 @@ function App() {
       return updatedTurns;
     });
   }
+  function handleRestart() {
+    setGameTurns([]);
+  }
+  function handlePlayerNameChange({ symbol, newName }) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName,
+      };
+    });
+  }
   return (
     <main>
       <div id="game-container">
@@ -65,17 +80,19 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(hasDraw || winner) && (
           <Gameover
             winner={winner}
-            hasDraw={hasDraw}
+            handleRestart={handleRestart}
           />
         )}
         <GameBoard
--- tic-tac-toe/src/components/GameOver.jsx
+++ tic-tac-toe/src/components/GameOver.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-export default function Gameover({ winner, hasDraw }) {
+export default function Gameover({ winner, handleRestart }) {
   return (
     <div id="game-over">
       <h2>Game Over</h2>
       {winner && <p>{winner} won!</p>}
       {!winner && <p>It&apos;s Draw</p>}
       <p>
-        <button>Rematch!</button>
+        <button onClick={handleRestart}>Rematch!</button>
       </p>
     </div>
   );

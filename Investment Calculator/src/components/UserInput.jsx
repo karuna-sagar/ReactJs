f660e85c70f87ac8f198dd1eb2af5ca7af4a7df9
Karuna sagar <karunasagar129130@gmail.com>
2025-01-01 23:39:05 +0530

    `Added state management and event handling to UserInput component`

--- Investment Calculator/src/components/UserInput.jsx
+++ Investment Calculator/src/components/UserInput.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 export default function UserInput() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturns: 6,
+    duration: 10,
+  });
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
   return (
     <section id="user-input">
       <div className="input-group">
@@ -7,13 +23,17 @@ export default function UserInput() {
           <input
             type="number"
             required
+            value={userInput.initialInvestment}
+            onChange={(e) => handleChange("initialInvestment", e.target.value)}
           />
         </p>
         <p>
-          <label>Actual Investment</label>
+          <label>Annual Investment</label>
           <input
             type="number"
             required
+            value={userInput.annualInvestment}
+            onChange={(e) => handleChange("annualInvestment", e.target.value)}
           />
         </p>
       </div>
@@ -23,6 +43,8 @@ export default function UserInput() {
           <input
             type="number"
             required
+            value={userInput.expectedReturns}
+            onChange={(e) => handleChange("expectedReturns", e.target.value)}
           />
         </p>
         <p>
@@ -30,6 +52,8 @@ export default function UserInput() {
           <input
             type="number"
             required
+            value={userInput.duration}
+            onChange={(e) => handleChange("duration", e.target.value)}
           />
         </p>
       </div>

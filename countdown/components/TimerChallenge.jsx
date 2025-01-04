23742f9214eea50d55835e3c4168972aed3d5fd0
Karuna sagar <karunasagar129130@gmail.com>
2025-01-04 11:38:06 +0530

    `Added state and functionality to track timer expiration and display a "You Lost" message`

--- countdown/components/TimerChallenge.jsx
+++ countdown/components/TimerChallenge.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timeStarted, setTimeStarted] = useState(false);
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimeStarted(true);
+  }
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You Lost</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={handleStart}>Start Challenge</button>
       </p>
       <p className="">Time is running... / Time is inactive</p>
     </section>

e462fe666488f3bbb85bd7a1005055608b5efc1a
Karuna sagar <karunasagar129130@gmail.com>
2025-01-04 10:40:08 +0530

    `Added input field and button to set player name, updated JSX to display entered name`

--- countdown/components/Player.jsx
+++ countdown/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  function handleName() {
+    setEnteredPlayerName(playerName.current.value);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2> {enteredPlayerName ?? "Welcome unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          type="text"
+          ref={playerName}
+        />
+        <button onClick={handleName}>Set Name</button>
       </p>
     </section>
   );

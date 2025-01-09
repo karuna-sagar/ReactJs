f0a9583f625ed124d473d6da283da9e51cd300bc
Karuna sagar <karunasagar129130@gmail.com>
2025-01-09 23:25:39 +0530

    basic sturcture of side bar

---
+++ task-management/src/components/ProjectsSidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function () {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}

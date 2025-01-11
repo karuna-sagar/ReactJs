d5138c5d2a89a6db9c496e10fda1c0e7845401e6
Karuna sagar <karunasagar129130@gmail.com>
2025-01-11 10:59:55 +0530

    `Refactor: Replace inline button with Button component from ./Button`

--- task-management/src/components/NewProject.jsx
+++ task-management/src/components/NewProject.jsx
@@ -1,3 +1,4 @@
+import Button from "./Button";
 import Input from "./Input";
 
 export default function NewProject() {
@@ -10,9 +11,7 @@ export default function NewProject() {
           </button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
-            Save
-          </button>
+          <Button>Save</Button>
         </li>
       </menu>
       <div>
---
+++ task-management/src/components/NoProjectSelected.jsx
@@ -0,0 +1,22 @@
+import noProjectImage from "../assets/no-projects.png";
+import Button from "./Button";
+export default function NoProjectSelected() {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        src={noProjectImage}
+        alt=""
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h1 className="text-3xl font-bold text-stone-500 my-4">
+        No Project Selected
+      </h1>
+      <p className="text-stone-400 mb-4">
+        Select a project or get started with new one{" "}
+      </p>
+      <p mt-8>
+        <Button>Create new project</Button>
+      </p>
+    </div>
+  );
+}

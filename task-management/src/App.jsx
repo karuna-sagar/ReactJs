0134db376c2183f3dd88a24ae447bc0d5ba5c575
Karuna sagar <karunasagar129130@gmail.com>
2025-01-11 17:23:49 +0530

    `Added functionality to start adding a new project from ProjectsSidebar and NoProjectSelected components`

--- task-management/src/App.jsx
+++ task-management/src/App.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    projects: [],
+    selectedProject: null,
+  });
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProject: null,
+      };
+    });
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
       {/* <NewProject /> */}
-      <NoProjectSelected />
+      <NoProjectSelected onStartAddProject={handleStartAddProject} />
     </main>
   );
 }
